d2e045e42a3401942fb3232f7945c3ee406289dc
Álvaro Márquez <marquezalvaro13@gmail.com>
2015-05-18 12:44:28 +0200

    Clase 29

---
+++ Clase29(Laravel)/instalacion.php
@@ -0,0 +1,7 @@
+<?php
+/*
+  Laravel se instala con composer a traves de la terminal:
+    1.- Creamos un proyecto: composer create-project laravel/laravel=4.2.* Clase29(Laravel) --prefer-dist
+        con --prefer-dist se descarga la version de produccion, no la de desarrollo
+*/
+?>
