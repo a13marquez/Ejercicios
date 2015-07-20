8eda6dd89ce413e0e4d102886eac35e4e8f069db
Álvaro Márquez <marquezalvaro13@gmail.com>
2015-07-20 11:56:20 +0200

    Prueba Laravel Clase 29

---
+++ PruebaLaravel/pruebalaravel/app/views/default.blade.php
@@ -0,0 +1,14 @@
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>@yield('titulo')</title>
+      <meta charset="utf-8">
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    </head>
+    <body>
+      @yield('contenido')
+    </body>
+  </html>
---
+++ PruebaLaravel/pruebalaravel/app/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('default')
+
+@section('titulo')
+  Mi primer proyecto
+
+@stop
+@section('contenido')
+  <div class=container>
+  <h1>Bienvendio a Laravel</h1>
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <a href="http://www.laravel.com">Página oficial</a>
+  @foreach ($usuarios as $usuario)
+    <p>{{$usuario}}</p>
+  @endforeach
+</div>
+@stop
