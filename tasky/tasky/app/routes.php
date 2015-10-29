2fe6f4f8cbf29b2a76baff326f23f820453439f5
Álvaro Márquez <amarquez@seidor.es>
2015-10-29 12:03:17 +0100

    Clase 49

--- tasky/tasky/app/routes.php
+++ tasky/tasky/app/routes.php
@@ -1,29 +1,41 @@
 <?php
+Route::pattern('id', '[0-9]+');
 
 
-Route::get('/', function(){return View::make('index');});
-Route::get('registro',array('before'=>'guest ', 'uses'=>'UserController@getRegistro'));
-Route::get('login',array('before'=>'guest', 'uses'=>'UserController@getlogin'));
-Route::get('lista',array('before'=>'auth','uses'=>'TaskController@getLista'));
-Route::get('iniciar/{id}',array('before'=>'auth','uses'=>'TaskController@getIniciar'));
-Route::get('completar/{id}',array('before' =>'auth','uses'=>'TaskController@getCompletar'));
-Route::get('eliminar/{id}',array('before' =>'auth','uses'=>'TaskController@getEliminar'));
-Route::get('recordar',array('before' =>'guest','uses'=>'RemindersController@getRemind'));
-Route::get('recuperar/{token}', array('before' =>'guest','uses'=>'RemindersController@getReset'));
-Route::get('idioma/{id}', function($id){
+Route::any('/', function(){return View::make('index');});
+Route::group(array('before'=>'guest '), function(){
+  Route::get('registro', 'UserController@getRegistro');
+  Route::get('login','UserController@getlogin');
+  Route::get('recordar','RemindersController@getRemind');
+  Route::get('recuperar/{token}','RemindersController@getReset');
+});
+
+Route::group( array('before'=>'auth'),function (){
+  Route::get('lista','TaskController@getLista');
+  Route::get('iniciar/{id}','TaskController@getIniciar')#->where('id', '[0-9]+');
+  Route::get('completar/{id}','TaskController@getCompletar')#->where('id', '[0-9]+');
+  Route::get('eliminar/{id}','TaskController@getEliminar')#->where('id', '[0-9]+');
+  });
+
+Route::grup(array('before'=>'guest|csrf'), function(){e
+  Route::post('registro' 'UserController@postRegistro');
+  Route::post('login','UserController@postLogin');
+  Route::post('recordar','RemindersController@postRemind');
+  Route::post('recuperar', 'RemindersController@postReset');
+});
+
+Route::grup(array('before' =>'auth|csrf')), function(){
+  Route::post('nueva','TaskController@postNueva');
+};
+
+Route::get('idioma/{idioma}', function($id){
   Session::put('lang', $id);
-  return Redirect::back();
-});
-/*
-Cambiar idioma con cookies
-Route::get('idioma/{id}', function($id){
-  Cookie::make('lang', $id, 525600);
-  return Redirect::back();
-});
-*/
-
-Route::post('registro' ,array('before'=>'guest|csrf', 'uses'=>'UserController@postRegistro'));
-Route::post('login',array('before'=>'guest|csrf', 'uses'=> 'UserController@postLogin'));
-Route::post('nueva',array('before' =>'auth|csrf','uses'=>'TaskController@postNueva'));
-Route::post('recordar', array('before' =>'guest|csrf','uses'=>'RemindersController@postRemind'));
-Route::post('recuperar', array('before' =>'guest|csrf','uses'=>'RemindersController@postReset'));
+    return Redirect::back();
+  })->where('idioma', '[a-z]+');
+  /*
+  Cambiar idioma con cookies
+  Route::get('idioma/{id}', function($id){
+    Cookie::make('lang', $id, 525600);
+    return Redirect::back();
+  });
+  */
